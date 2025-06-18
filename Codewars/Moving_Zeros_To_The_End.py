e79206f5cab1c23378245b5b7f9da6b94d5e1d3f
ArthurViniDev <kenzygamedev@gmail.com>
2025-06-17 23:54:35 -0300

    Update Moving_Zeros_To_The_End.py

--- Codewars/Moving_Zeros_To_The_End.py
+++ Codewars/Moving_Zeros_To_The_End.py
@@ -6,3 +6,4 @@ def move_zeros(lst: list[int]) -> list[int]:
             no_zeros.append(lst[i])
     zeros_list = [0] * (len(lst) - len(no_zeros))
     return no_zeros + zeros_list
+# Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements. 
